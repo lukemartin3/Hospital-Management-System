23887f3de20e5a8173466a1f80413772827516af
amrohwer <abrahm-rohwer@uiowa.edu>
2023-02-13 17:32:26 -0600

    Iteration 0

---
+++ Starter.py
@@ -0,0 +1,53 @@
+import getpass
+import oracledb
+
+pw = getpass.getpass("Enter password: ")
+
+connection = oracledb.connect(
+    user="system",
+    password=pw,
+    dsn="localhost/xe")
+
+print("Successfully connected to Oracle Database")
+
+cursor = connection.cursor()
+
+# Create a table
+
+cursor.execute("""
+    begin
+        execute immediate 'drop table todoitem';
+        exception when others then if sqlcode <> -942 then raise; end if;
+    end;""")
+
+cursor.execute("""
+    create table todoitem (
+        id number generated always as identity,
+        description varchar2(4000),
+        creation_ts timestamp with time zone default current_timestamp,
+        done number(1,0),
+        primary key (id))""")
+
+# Insert some data
+
+rows = [ ("Task 1", 1 ),
+         ("Task 2", 1 ),
+         ("Task 3", 1 ),
+         ("Task 4", 1 ),
+         ("Task 5", 0 ) ]
+
+cursor.executemany("insert into todoitem (description, done) values(:1, :2)", rows)
+print(cursor.rowcount, "Rows Inserted")
+
+connection.commit()
+
+# Now query the rows back
+for row in cursor.execute('select description, done from todoitem'):
+    if (row[1]):
+        print(row[0], "is done")
+    else:
+        print(row[0], "is NOT done")
+
+res = [lis[1] for lis in rows]
+for i in res:
+    assert i == 1, "is NOT done"
