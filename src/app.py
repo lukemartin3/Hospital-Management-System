32a20a5a4c0139c88fab52ce4d98207933f65fd6
Zellenon <Zellenon@gmail.com>
2023-03-27 17:36:26 -0500

    added basic session files

--- src/app.py
+++ src/app.py
@@ -1,12 +1,32 @@
-from flask import Flask
-from markupsafe import escape
-
+from flask import Flask, render_template, redirect, request, session
+# Always use Flask.session instead of the Session object for direct access.
+from flask_session import Session
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config['SECRET_KEY'] = "debug key" 
+app.config['SESSION_TYPE'] = 'filesystem' 
+app.config['SESSION_PERMANENT']= False
+Session(app)
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/")
+def index():
+    if not session.get("user_name"):
+        return redirect("/login")
+    return render_template('welcome.html')
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session["user_name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["user_name"] = None
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
